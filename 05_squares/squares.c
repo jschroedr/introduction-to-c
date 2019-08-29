5557fd6d149941e77cc8ccb8a68c00e1e72dc3b6
Coursera Learner <nobody@nowhere.nul>
2019-08-29 00:52:33 +0000

    Assignment 5

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -7,9 +7,11 @@
  * offset (INCLUSIVE) and offset + size (EXCLUSIVE)
  */
 int isInRange(int coord, int offset, int size) {
-  // if coord is in range, return 1
-  // else, return 0
-  return 0;
+  if ((coord >= offset) && (coord < (offset + size))) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 /*
@@ -17,18 +19,54 @@ int isInRange(int coord, int offset, int size) {
  * offset + size
  */
 int isAtBorder(int coord, int offset, int size) {
-  // if coord is equal to offest or offset + size
-  // return 1, else return 0
-  return 0;
+  if((coord == offset) || (coord == (offset + size - 1))){
+    return 1;
+  } else {
+    return 0;
+  }
 }
+
+int starCheck(int x, int x_offset, int y, int y_offset, int size2) {
+  int xCheck = isInRange(x, x_offset, size2);
+  int yCheck = isAtBorder(y, y_offset, size2);
+  if (xCheck && yCheck) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int hashCheck(int x, int y, int size1){
+  if((x < size1) && (y == 0 || y == (size1 - 1))) {
+      return 1;
+    } else {
+      return 0;
+    }
+}
+
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-
+  int w = size1 + (x_offset + size2);
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int h = size1 + (y_offset + size2);
   //count from 0 to h. Call the number you count with y
-
+  for(int y = 0; y <= h; y ++){
+    for(int x = 0; x <= w; x++) {
+      int checkOneStar = starCheck(x, x_offset, y, y_offset, size2);
+      int checkTwoStar = starCheck(y, y_offset, x, x_offset, size2);
+      int checkOneHash = hashCheck(x, y, size1);
+      int checkTwoHash = hashCheck(y, x, size1);
+      if(checkOneStar || checkTwoStar){
+	printf("*");
+      } else if(checkOneHash || checkTwoHash) {
+	printf("#");
+      } else {
+	printf(" ");
+      }
+    }
+    printf("\n");
+  }
     //count from 0 to w. Call the number you count with x
 
       //check if  EITHER
