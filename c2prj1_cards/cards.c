7a59b7bd8286423abc1fb1ef5878445e51ad4f84
Coursera Learner <nobody@nowhere.nul>
2019-09-20 02:16:48 +0000

    Assn cards

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,34 +5,169 @@
 
 
 void assert_card_valid(card_t c) {
-
-
+  // assert() to check that the card passd in has valid values
+  // between 2 and VALUE_ACE (inclusive)
+  assert(c.value >= 2);
+  assert(c.value <= VALUE_ACE);
+  // suit should be between SPADES and CLUBS
+  assert(c.suit >= SPADES);
+  assert(c.suit <= CLUBS);
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
-  return "";
+  // convert the hand_ranking_t enumerated value into a string
+  // emacs keyboard macros might come in handy (see Drew's lesson)
+  assert(r <= 8);
+  assert(r >= 0);
+  switch(r)
+    {
+    case 0:
+      return "STRAIGHT_FLUSH";
+    case 1:
+      return "FOUR_OF_A_KIND";
+    case 2:
+      return "FULL_HOUSE";
+    case 3:
+      return "FLUSH";
+    case 4:
+      return "STRAIGHT";
+    case 5:
+      return "THREE_OF_A_KIND";
+    case 6:
+      return "TWO_PAIR";
+    case 7:
+      return "PAIR";
+    case 8:
+      return "NOTHING";
+    default:
+      return "";
+    }
 }
 
 char value_letter(card_t c) {
-  return 'x';
+  // return the character that textually represents the value of the passed-in card
+  if (c.value < 10) {
+    return c.value + 48;
+  } else if (c.value == 10) {
+    return 48;
+  } else {
+    switch(c.value)
+      {
+      case VALUE_JACK:
+	return 74; // J
+      case VALUE_QUEEN:
+	return 81; // Q
+      case VALUE_KING:
+	return 75; // K
+      case VALUE_ACE:
+	return 65; // A
+      default:
+	return 0;
+      }
+  }
+  // for values 2-9, this should be that digit
+  // for 10 it should be 0
+  // for jack, queen, king and ace, it should be j, q, k and a
+  // remember everything is a number
+  // for example, 0 has the decimal value 48
+  // and 5 has the value 53
+  // so '0' + 5 = '5'
+  
 }
 
 
 char suit_letter(card_t c) {
-  return 'x';
-  
+  // return the letter that textually represetns the suit of the card passed in
+  // ('s', 'h', 'd', or 'c' for SPADES, HEARTS, DIAMONS or CLUBS
+  switch(c.suit)
+    {
+    case 0:
+      return 115; // SPADES
+    case 1:
+      return 104; // HEARTS
+    case 2:
+      return 100; // DIAMONDS
+    case 3:
+      return 99; // CLUBS
+    default:
+      return 0;
+    }
 }
 
 void print_card(card_t c) {
-
+  // print out the textual representation of the card
+  // hint: use the functions your previously wrote
+  // print the value first, then the suit
+  // As = Ace of Spades
+  // NOTE: do note print any spaces or newlines after the card's text
+  char value = value_letter(c);
+  char suit = suit_letter(c);
+  printf("%c%c", value, suit);
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
+  // make and return a cart_t whoe value and suit correspond to the letters passed in
+  // if the values passed in are invalid
+  // use assert() or print an error message and exit(EXIT_FAILURE)
   card_t temp;
+  // turn value_let into a number
+  if (value_let <= 57) {  // value_let is a number
+    temp.value = value_let - 48;
+  } else {
+    switch (value_let)
+      {
+      case 74:
+	temp.value = VALUE_JACK;
+	break;
+      case 81:
+	temp.value = VALUE_QUEEN;
+	break;
+      case 75:
+	temp.value = VALUE_KING;
+	break;
+      case 65:
+	temp.value = VALUE_ACE;
+	break;
+      default:
+	temp.value = -1;  // INVALID
+      }
+  }
+  // get the int value from the letter for suit
+  switch(suit_let)
+    {
+    case 115:
+      temp.suit = 0;  // SPADES
+      break;
+    case 104:
+      temp.suit = 1; // HEARTS
+      break;
+    case 100:
+      temp.suit = 2; // DIAMONDS
+      break;
+    case 99:
+      temp.suit = 3; // CLUBS
+      break;
+    default:
+      temp.suit = -1; // INVALID
+      break;
+    }
+  assert_card_valid(temp);
   return temp;
 }
 
 card_t card_from_num(unsigned c) {
+  // take a number from 0 to 52 (excusive)
+  // map the number uniquely to a crd value/suit combination
+  // exactly how you map the number to values/suits is up to you
+  // but you must guarantee that each valid value/suit combination
+  // corresponds to exactly one input value in the range [0, 52]
+  // Hint: you may want to use the mod operator % to find the remainder of a number divided by 13
+  // in course 3, this function will be used to make a deck of cards by iterating over that range
+  // and calling it once for each value -- you just need to learn about arrays first so you
+  // have a place to put all those cards
   card_t temp;
+  temp.suit  = c / 13;  // integer division to determine suit
+  temp.value = c - (13 * temp.suit);
+  temp.value = (temp.value + 2);  // lowest card is 2, highest is ace
   return temp;
 }
--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,78 @@
 #include "cards.h"
+#include "stdio.h"
+#include "assert.h"
+
+void test_assert_card_valid() {
+  card_t validCard;
+  validCard.value = 2;
+  validCard.suit = SPADES;
+  printf("Now testing a valid card\n");
+  assert_card_valid(validCard);
+  printf("\n----------------------------\n");
+  
+  /*
+  card_t invalidCardSuitH;
+  invalidCardSuitH.value = 2;
+  invalidCardSuitH.suit = 4;  // max should be 3
+  printf("Testing Invalid Card with Suit too High\n");
+  assert_card_valid(invalidCardSuitH);
+  printf("\n---------------------------------\n");
+  */
+
+  /*
+  card_t invalidCardSuitL;
+  invalidCardSuitL.value = 2;
+  invalidCardSuitL.suit = -1;
+  printf("Testing invalid card with suit too low\n");
+  assert_card_valid(invalidCardSuitL);
+  printf("\n-------------------------------\n");
+  */
+  /*
+  card_t invalidCardValueH;
+  invalidCardValueH.value = 15;
+  invalidCardValueH.suit = 2;
+  assert_card_valid(invalidCardValueH);
+  */
+  /*
+  card_t invalidCardValueL;
+  invalidCardValueL.value = 1;
+  invalidCardValueL.suit = 2;
+  assert_card_valid(invalidCardValueL);
+  */
+}
+
 
 int main(void) {
+  /*
+  // test_assert_card_valid();
 
+  // ranking_to_string(STRAIGHT_FLUSH);
+  // ranking_to_string(NOTHING);
+  // printf("%d", STRAIGHT_FLUSH);
+  // printf("%d", NOTHING);
+
+  card_t myCard;
+  myCard.value = 13;
+  myCard.suit = 1;
+  char myLetter = value_letter(myCard);
+  printf("\n%d\n", myLetter);
+  printf("\n%c\n", myLetter);
+
+  char mySuite = suit_letter(myCard);
+  printf("%c\n", mySuite);
+
+  print_card(myCard);
+  */
+  /*
+  char testValue = 'A';
+  char testSuit = 's';
+  card_t testCardFromL = card_from_letters(testValue, testSuit);
+  print_card(testCardFromL);
+  */
+  unsigned myNum;
+  myNum = 51;
+  card_t testCardFromN = card_from_num(myNum);
+  printf("\n%d", testCardFromN.value);
+  printf("\n%d\n\n", testCardFromN.suit);
+  print_card(testCardFromN);
 }
