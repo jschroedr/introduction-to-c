4a43bf1f1eaab383133984a773d19fe57d36f94e
Coursera Learner <nobody@nowhere.nul>
2019-09-21 17:13:09 +0000

    20190921.7

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -101,8 +101,9 @@ void print_card(card_t c) {
   // As = Ace of Spades
   // NOTE: do note print any spaces or newlines after the card's text
   char value = value_letter(c);
+  printf('%c', value); 
   char suit = suit_letter(c);
-  printf("%c%c", value, suit);
+  printf('%c', suit);
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
