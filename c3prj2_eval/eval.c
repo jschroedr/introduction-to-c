20c907b60060becae27cd3795742a821af66e15f
Coursera Learner <nobody@nowhere.nul>
2019-12-30 22:39:40 +0000

    test

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -394,9 +394,6 @@ hand_eval_t build_hand_from_match(deck_t * hand,
  * 
  */
 int compare_hands(deck_t * hand1, deck_t * hand2) {
-  print_hand(hand1);
-  printf("-----------\n");
-  print_hand(hand2);
   // sort each hand DESC using our comparison function
   qsort(hand1->cards, hand1->n_cards, sizeof(hand1->cards[0]), card_ptr_comp);
   qsort(hand2->cards, hand2->n_cards, sizeof(hand2->cards[0]), card_ptr_comp);
