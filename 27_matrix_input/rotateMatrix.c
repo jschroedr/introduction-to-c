b08024e547f9c901c5514513d5ca6a62e75a8644
Coursera Learner <nobody@nowhere.nul>
2020-01-26 02:31:51 +0000

    stopping point, assn 27

---
+++ 27_matrix_input/rotateMatrix.c
@@ -0,0 +1,28 @@
+// imports
+
+
+int main() {
+
+
+  
+
+  return EXIT_SUCCESS;
+}
+
+// one command line argument, a string specifying input file to read
+// test: right number of arguments?
+// test: file found and able to be opened for reading?
+
+// the input file should contain ten lines
+// each has 10 non-newline characters (plus a newline)
+// test: test each 1-10 character to see if it is newline
+// test: test that we only get 10 lines
+// test: DO NOT cast the character to a char, check if it is EOF first
+
+// use the rotate function we already wrote to rotate in input matrix
+// set the related assignment
+// print the rotated result on stdout
+
+
+// if there are any errors, your program should
+// print an error message to stderr and exit with exit failure
