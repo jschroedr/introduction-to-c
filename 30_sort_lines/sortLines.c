235276d199db4a799a5c5d454a2c38f4d069266e
Coursera Learner <nobody@nowhere.nul>
2020-02-16 23:47:17 +0000

    Assn 30, b6

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -73,9 +73,6 @@ int main(int argc, char ** argv) {
 	lineArray[lineCount - 1] = malloc((strlen(line) + 1) * sizeof(lineArray[lineCount - 1]));
 	strcpy(lineArray[lineCount - 1], line);
 	lineCount ++;
-	if(nread == 1) {
-	  break;
-	}
       }
       free(line);
       // sort the lines
