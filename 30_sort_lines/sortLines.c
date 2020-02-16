c0edd311db17540d57617784c376db9d7c049e83
Coursera Learner <nobody@nowhere.nul>
2020-02-16 22:41:41 +0000

    Assn 30, b1

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -16,9 +16,73 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+
+void printSortResult(char ** data, size_t count) {
+  for (int i = 0; i < (int)count; i ++) {
+    printf("%s\n", data[i]);
+  }
+}
+
+
+void freeLineArray(char ** data, size_t count) {
+  for (int i = 0; i < (int)count; i ++) {
+    free(data[i]);
+  }
+  free(data);
+}
+
+
 int main(int argc, char ** argv) {
-  
-  //WRITE YOUR CODE HERE!
-  
+  size_t len = 0;
+  ssize_t nread;
+  char * line = NULL;
+  char ** lineArray = NULL;
+  size_t lineCount;
+  int totalFiles = argc - 1;
+  if (totalFiles == 0) {
+    lineCount = 1;
+    while ((nread = getline(line, len, stdin)) != -1) {
+      lineArray = realloc(lineArray, (lineCount * sizeof(lineArray)));
+      lineArray[lineCount - 1] = line;
+      lineCount ++;
+    }
+    // sort the array of strings
+    sortData(lineArray, lineCount);
+    // print the array of strings
+    printSortResult(lineArray, lineCount);
+    // free the array of strings
+    freeLineArray(lineArray, lineCount);
+    return EXIT_SUCCESS;
+  } else {  // argc > 1
+    for (int i = 0; i < totalFiles; i ++) {
+      // treat each argument as an input file name
+      // open the file
+      FILE * targetFile = fopen(argv[i], "r");
+      if (targetFile == NULL) {
+	fprintf(stderror, "File not found");
+	return EXIT_FAILURE;
+      }
+      lineCount = 1;
+      // read all the lines of data in it
+      while (nread = getline(line, len, targetFile) != -1) {
+	if (nread == 0) {
+	  break;
+	}
+	lineArray = realloc(lineArray, (lineCount * sizeof(lineArray)));
+	lineArray[lineCount - 1] = line;
+	lineCount ++;
+      }
+      // sort the lines
+      sortData(lineArray, lineCount);
+      // print the results (TODO)
+      printSortResult(lineArray, lineCount);
+      // free the memory
+      freeLineArray(lineArray, lineCount);
+      // close the file
+      if (fclose(targetFile) != 0) {
+	perror("Unable to close input file");
+	return EXIT_FAILURE;
+      }
+  }
   return EXIT_SUCCESS;
 }
